b65052d9f5569deaf028229f6e387b834b920c10
Milosz08 <gilgamilosz451@gmail.com>
2021-06-25 03:15:37 +0200

    25/06/2021 3:15 AM

---
+++ cmake-build-debug/CMakeFiles/PK_Matrix_Calculator.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PK_Matrix_Calculator.dir/Program_Interface/src/initObjects/chooseMtrxType.cpp.obj"
+  "CMakeFiles/PK_Matrix_Calculator.dir/Program_Interface/src/initObjects/chooseNmbrType.cpp.obj"
+  "CMakeFiles/PK_Matrix_Calculator.dir/Program_Interface/src/initObjects/initMtrxObj.cpp.obj"
+  "CMakeFiles/PK_Matrix_Calculator.dir/Program_Interface/src/initObjects/setMtrxSize.cpp.obj"
+  "CMakeFiles/PK_Matrix_Calculator.dir/Program_Interface/src/mainMenu.cpp.obj"
+  "CMakeFiles/PK_Matrix_Calculator.dir/Program_Interface/src/startPrg.cpp.obj"
+  "CMakeFiles/PK_Matrix_Calculator.dir/main.cpp.obj"
+  "PK_Matrix_Calculator.exe"
+  "PK_Matrix_Calculator.exe.manifest"
+  "PK_Matrix_Calculator.pdb"
+  "libPK_Matrix_Calculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PK_Matrix_Calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
